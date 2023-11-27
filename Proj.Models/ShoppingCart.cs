a92787f6907c3cef9330ca3a5bb856c023965fb0
MBoota <m43577535@gmail.com>
2023-11-28 00:52:31 +0500

    adding_ShoppingCart

---
+++ Proj.Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.Models
+{
+    public class ShopingCart
+    {
+        public int Id { get; set; } 
+        public int productId { get; set; }
+        [ForeignKey("productId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        public string Count { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
