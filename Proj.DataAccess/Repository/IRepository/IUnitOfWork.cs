6be47450c44e61f2ce212d2c08524f9dee12e80c
MBoota <m43577535@gmail.com>
2023-11-28 07:17:07 +0500

    Creating ApplicationUser Repo In case if required crud

--- Proj.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Proj.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proj.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Proj.DataAccess.Repository.IRepository
         public IProductRepository Product { get; }
         public ICompanyRepository Company { get; }
         public IShoppingCartRepository ShoppingCart { get; }
+        public IApplicationUserRepository ApplicationUser { get; }
         void SaveChange();
     }
 }
