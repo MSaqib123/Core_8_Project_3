02bdff2c2b7d405ce9031b2c2155848f5a480671
MBoota <m43577535@gmail.com>
2023-11-29 18:55:22 +0500

    adding_Detail

--- Proj.Web/Models/ErrorViewModel.cs
+++ Proj.Web/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Proj.Web.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
