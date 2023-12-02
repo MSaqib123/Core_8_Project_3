64983a7f5a63b71d82083dcda1d865c5dbc5e59d
MBoota <m43577535@gmail.com>
2023-12-02 19:31:18 +0500

    Adding Cart link in layout

--- Proj.Web/Areas/Customer/Controllers/CartController.cs
+++ Proj.Web/Areas/Customer/Controllers/CartController.cs
@@ -3,14 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Proj.Web.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-
-
     }
 }
