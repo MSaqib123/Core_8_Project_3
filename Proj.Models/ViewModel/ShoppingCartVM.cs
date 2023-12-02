94a220127e69495c615fc4e0dc393510422de10b
MBoota <m43577535@gmail.com>
2023-12-02 20:15:53 +0500

    Adding  ShoppingCartVM

---
+++ Proj.Models/ViewModel/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.Models.ViewModel
+{
+    internal class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public float OrderTotal { get; set; }
+    }
+}
