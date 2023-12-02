aa7f16d749423065858bf9bd91a467cdfb4a70e6
MBoota <m43577535@gmail.com>
2023-12-02 19:26:26 +0500

    Adding _Cart Contlere

---
+++ Proj.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proj.Web.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
