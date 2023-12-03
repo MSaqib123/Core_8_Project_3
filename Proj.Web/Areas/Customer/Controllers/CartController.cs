937ee93f7db8d7e776b434cee094521e0e738810
MBoota <m43577535@gmail.com>
2023-12-04 01:51:02 +0500

    working on Carts

--- Proj.Web/Areas/Customer/Controllers/CartController.cs
+++ Proj.Web/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Proj.DataAccess.Repository.IRepository;
 using Proj.Models.ViewModel;
 
 namespace Proj.Web.Areas.Customer.Controllers
@@ -9,9 +10,15 @@ namespace Proj.Web.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork iUnit;
+        public CartController(IUnitOfWork _iUnit)
+        {
+            iUnit = _iUnit;
+        }
         public IActionResult Index()
         {
             ShoppingCartVM vm = new ShoppingCartVM();
+            vm.ShoppingCartList = iUnit.ShoppingCart.GetAll();
             return View();
         }
     }
