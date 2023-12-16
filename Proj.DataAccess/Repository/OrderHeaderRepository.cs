29da6c7fc7b90cc03e1d9a343d550461b4e8d01c
MBoota <m43577535@gmail.com>
2023-12-16 14:57:33 +0500

    Adding UpdateStatus method, UpdateStripePayment method

--- Proj.DataAccess/Repository/OrderHeaderRepository.cs
+++ Proj.DataAccess/Repository/OrderHeaderRepository.cs
@@ -1,4 +1,5 @@
-﻿using Proj.DataAccess.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using Proj.DataAccess.Data;
 using Proj.DataAccess.Repository.IRepository;
 using Proj.Models;
 using System;
@@ -20,6 +21,33 @@ namespace Proj.DataAccess.Repository
         public void Update(OrderHeader obj)
         {
             _db.orderHeaders.Update(obj);
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _db.orderHeaders.FirstOrDefault(x=>x.Id == id);
+            if (orderFromDb!= null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentId(int id, string sessionId, string paymentIntendId)
+        {
+            var orderFromDb = _db.orderHeaders.FirstOrDefault(x => x.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntendId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntendId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
         }
     }
 }
