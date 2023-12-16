d132f78ca03db6c65d2ef2b35600ec1963244c04
MBoota <m43577535@gmail.com>
2023-12-17 02:32:27 +0500

    Adding OrderController

---
+++ Proj.Web/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proj.DataAccess.Repository.IRepository;
+
+namespace Proj.Web.Areas.Customer.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork iUnit;
+        public OrderController(IUnitOfWork _iUnit)
+        {
+            iUnit = _iUnit;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //_______________________ APis _______________________
+        #region Apis work
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = iUnit.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = list });
+        }
+
+        #endregion
+    }
+}
