e88ad4ba9b440d3d38fafeac9532f0c707628557
MBoota <m43577535@gmail.com>
2023-12-16 09:06:03 +0500

    Adding  StaticDetail for  Status

--- Proj.Utility/SD.cs
+++ Proj.Utility/SD.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace Proj.Utility
 {
+    //static detail files
     public static class SD
     {
         public const string Role_Customer = "Customer";
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
+
+        //_______ Order Status __________
+        public static string StatusPending = "Pending";
+        public static string StatusApproved = "Approved";
+        public static string StatusInProcess = "Proccessing";
+        public static string StatusShipped = "Shipped";
+        public static string StatusCancelled = "Cancelled";
+        public static string StatusRefunded = "Refunded";
+
+        public static string PaymentStatusPending = "Pending";
+        public static string PaymentStatusApproved = "Approved";
+        public static string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public static string PaymentStatusRejected = "Rejected";
     }
 }
