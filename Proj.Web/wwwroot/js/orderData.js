5b95a961932ffb8983ca1cf9759ae8319074b0c3
MBoota <m43577535@gmail.com>
2023-12-17 12:42:19 +0500

    1. Adding link in View 2. Modified Js

--- Proj.Web/wwwroot/js/orderData.js
+++ Proj.Web/wwwroot/js/orderData.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
         dataTable = $('#tblData').DataTable({
             "ajax": { url: '/Admin/Company/GetAll' },
             "columns": [
-                { data: 'id' },
-                { data: 'name' },
-                { data: 'city' },
-                { data: 'state' },
-                { data: 'postalCode' },
+                { data: 'id' , "width":"10%"},
+                { data: 'name', "width": "20%" },
+                { data: 'phonenumber', "width": "25%" },
+                { data: 'applicationuser.email', "width": "20%" },
+                { data: 'orderstatus', "width": "15%" },
+                { data: 'ordertotal', "width": "10%" },
                 {
                     data: 'id',
                     "render": function (data) {
                         return `
                             <a href="/Admin/Company/Upsert?id=${data}" class="btn btn-primary">Edit</a>
-                            <a onClick=Delete("/Admin/Company/DeleteRecord?id=${data}") class="btn btn-danger">Delete</a>
                         `}
                 },
             ]
@@ -24,34 +24,3 @@ $(document).ready(function () {
     }
 
 });
-
-
-
-function Delete(url)
-{
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'delete',
-                success: function (response) {
-                    dataTable.ajax.reload();
-                    toastr.success(response.message);
-                },
-                error: function (response) {
-                    toastr.error(response.message);
-                }
-
-            })
-            
-        }
-    })
-}
