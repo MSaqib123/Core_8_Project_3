dad36f3920145dc9221096cd7b6806add287f792
MBoota <m43577535@gmail.com>
2023-12-17 13:36:57 +0500

    Adding Filter logic in API request

--- Proj.Web/Areas/Admin/Controllers/OrderController.cs
+++ Proj.Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Proj.DataAccess.Repository.IRepository;
+using Proj.Models;
+using Proj.Utility;
 
 namespace Proj.Web.Areas.Admin.Controllers
 {
@@ -21,9 +23,31 @@ namespace Proj.Web.Areas.Admin.Controllers
         //_______________________ APis _______________________
         #region Apis work
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            var list = iUnit.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> list = iUnit.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+                    list = list.Where(x => x.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+
+                case "inprocess":
+                    list = list.Where(x => x.OrderStatus == SD.StatusInProcess);
+                    break;
+
+                case "completed":
+                    list = list.Where(x => x.PaymentStatus == SD.StatusShipped);
+                    break;
+
+                case "approved":
+                    list = list.Where(x => x.PaymentStatus == SD.StatusApproved);
+                    break;
+
+                default:
+                    break;
+
+            }
             return Json(new { data = list });
         }
 
