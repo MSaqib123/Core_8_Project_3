0e2a0f2e3f15f5ccea6ebce561ca45a9f15198c7
MBoota <m43577535@gmail.com>
2023-12-17 13:45:13 +0500

    Adding Filter with Datatable and .net to dynamic changes in data

--- Proj.Web/wwwroot/js/orderData.js
+++ Proj.Web/wwwroot/js/orderData.js
@@ -1,26 +1,47 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTabel();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTabel("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTabel("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTabel("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTabel("approved");
+                }
+                else {
+                    loadDataTabel("all");
+                }
+            }
+        }
+    }
+    
+});
 
-    function loadDataTabel() {
-        dataTable = $('#tblData').DataTable({
-            "ajax": { url: '/Admin/Order/GetAll' },
-            "columns": [
-                { data: 'id' , "width":"10%"},
-                { data: 'name', "width": "20%" },
-                { data: 'phoneNumber', "width": "20%" },
-                { data: 'applicationUser.email', "width": "20%" },  
-                { data: 'orderStatus', "width": "15%" },
-                { data: 'orderTotal', "width": "10%" },
-                {
-                    data: 'id',
-                    "render": function (data) {
-                        return `
+function loadDataTabel(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/Order/GetAll?status=' + status },
+        "columns": [
+            { data: 'id', "width": "10%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "20%" },
+            { data: 'orderStatus', "width": "15%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
                             <a href="/Admin/Order/Upsert?id=${data}" class="btn btn-primary">Edit</a>
                         `}
-                },
-            ]
-        });
-    }
-
-});
+            },
+        ]
+    });
+}
