72c217845520b9efed5dc63e35a0ac9d79ff57be
MBoota <m43577535@gmail.com>
2023-12-21 01:44:14 +0500

    Order payment pay is successfully done by Company

--- Proj.Web/Areas/Admin/Controllers/OrderController.cs
+++ Proj.Web/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Proj.Models;
 using Proj.Models.ViewModel;
 using Proj.Utility;
 using Stripe;
+using Stripe.Checkout;
 using Stripe.Climate;
 using System.Security.Claims;
 
@@ -93,7 +94,6 @@ namespace Proj.Web.Areas.Admin.Controllers
             return RedirectToAction(nameof(Details), new { orderId = orderVM.OrderHeader.Id });
         }
 
-
         [HttpPost]
         [Authorize(Roles = SD.Role_Employee + " , " + SD.Role_Admin)]
         public IActionResult CancelOrder()
@@ -117,6 +117,77 @@ namespace Proj.Web.Areas.Admin.Controllers
             iUnit.SaveChange();
             TempData["Success"] = "Order Cancelled Successfully";
             return RedirectToAction(nameof(Details), new { orderId = orderVM.OrderHeader.Id });
+        }
+
+        [ActionName("Details")]
+        [HttpPost]
+        public IActionResult Details_Pay_NOW()
+        {
+            orderVM.OrderHeader = iUnit.OrderHeader.Get(x => x.Id == orderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+            orderVM.OrderDetail = iUnit.OrderDetail.GetAll(x => x.OrderHeaderId == orderVM.OrderHeader.Id, includeProperties: "Product").ToList();
+
+            //its regular Customer account and need to Capture payment
+            //stripe logic
+            var domain = "http://localhost:5003/";
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                SuccessUrl = domain + $"Admin/Order/PaymentConfirmation?orderHeaderId={orderVM.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/Order/details?orderId={orderVM.OrderHeader.Id}",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+            };
+
+            //___ iterate all item in strape __
+            foreach (var item in orderVM.OrderDetail)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)item.Price * 100,
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title,
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+            var service = new Stripe.Checkout.SessionService();
+            Session session = service.Create(options);
+            
+            iUnit.OrderHeader.UpdateStripePaymentId(orderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            iUnit.SaveChange();
+
+            //____ Redirect ____
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+
+        }
+
+        public IActionResult PaymentConfirmation(int orderHeaderId)
+        {
+            OrderHeader orderHeader = iUnit.OrderHeader.Get(u => u.Id == orderHeaderId, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                //this is Company Order
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    iUnit.OrderHeader.UpdateStripePaymentId(orderHeaderId, session.Id, session.PaymentIntentId);
+                    iUnit.OrderHeader.UpdateStatus(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    iUnit.SaveChange();
+                }
+            }
+            //___________ Removing All Showiping Cart List for DB _________
+            List<ShoppingCart> shopingCart = iUnit.ShoppingCart.GetAll(x => x.ApplicationUserId == orderHeader.ApplicationUserID).ToList();
+            iUnit.ShoppingCart.RemoveRange(shopingCart);
+            iUnit.SaveChange();
+
+            return View(orderHeaderId);
         }
 
         //_______________________ APis _______________________
