e81fd4e81a9b02374619ad757c28b1b853aca5b9
MBoota <m43577535@gmail.com>
2023-12-23 20:04:06 +0500

    Adding Db Iniitilizer

---
+++ Proj.DataAccess/DbInitilizer/DbInitilizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.DataAccess.DbInitilizer
+{
+    public class DbInitilizer : IDbInitilizer
+    {
+        public void Initilize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
