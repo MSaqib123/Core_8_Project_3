40c0ff28f953462c1d2060313fd520e703fb7931
MBoota <m43577535@gmail.com>
2023-12-23 21:07:03 +0500

    Adding Users Management for Admin and Employee 1. adding controller 2. View

---
+++ Proj.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proj.DataAccess.Data;
+using Proj.DataAccess.Repository.IRepository;
+using Proj.Models;
+using Proj.Utility;
+
+namespace Proj.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController: Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public UserController(ApplicationDbContext _db)
+        {
+            db= _db;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //_______________________ APis _______________________
+        #region Apis work
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = db.ApplicationUsers.Include(x=>x.Company).ToList();
+            return Json(new {data= list });
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteRecord(int? id)
+        {
+            return Json(new { success = true, message = "Deleted Successfully" });
+        }
+
+        #endregion
+    }
+}
