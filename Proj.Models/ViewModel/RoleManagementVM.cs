5ba84bf6fb47c45b7f1ecac3cdc38d6116d8efa7
MBoota <m43577535@gmail.com>
2023-12-24 20:59:23 +0500

    Adding RoleManagmentVM

---
+++ Proj.Models/ViewModel/RoleManagementVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.Models.ViewModel
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser applicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+
+        //___ Alredy in  ApplicationUser ___
+        //public string CurrentRole { get; set; }
+    }
+}
