3f5bbb2c5fa42ac5e4223933171d05ac9b76134e
MBoota <m43577535@gmail.com>
2023-12-24 09:54:22 +0500

    Adding UserData.js file

---
+++ Proj.Web/wwwroot/js/userdata.js
@@ -0,0 +1,24 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTabel();
+});
+
+function loadDataTabel() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/User/GetAll'},
+        "columns": [
+            { data: 'name', "width": "20%" },
+            { data: 'email', "width": "20%" },
+            { data: 'phone', "width": "20%" },
+            { data: 'company.name', "width": "15%" },
+            { data: '', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                            <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary">Edit</a>
+                        `}
+            },
+        ]
+    });
+}
