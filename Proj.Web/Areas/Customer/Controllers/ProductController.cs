2346c2c106f6c00b5d3def7edbdd850c51086778
MBoota <m43577535@gmail.com>
2024-01-07 14:47:47 +0500

    Implementing Iunit in User controller previusly we used only _db

--- Proj.Web/Areas/Customer/Controllers/ProductController.cs
+++ Proj.Web/Areas/Customer/Controllers/ProductController.cs
@@ -20,10 +20,6 @@ namespace Proj.Web.Areas.Customer.Controllers
         {
             return View();
         }
-
-
-
-
         public IActionResult Detail(int id)
         {
             ShoppingCart s = new ShoppingCart();
